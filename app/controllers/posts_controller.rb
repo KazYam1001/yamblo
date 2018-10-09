d10d2c1d2fdd81ab2b52dea33079e8cb803a8561
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-09 20:45:40 +0900

    新規投稿機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,6 +26,10 @@ class PostsController < ApplicationController
   end
 
   private
+  def post_params
+    params.require(:post).permit(:title, :image, :body)
+  end
+
   def set_post
     @post = Post.find(params:[:id])
   end
