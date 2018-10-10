e399ae7b6bcd036ff03057c9b17b730747a39f6b
Kazushi Yamanishi <keironia187149@gmail.com>
2018-10-10 12:32:14 +0900

    記事の詳細ページを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
+  end
+
+  def show
   end
 
   def new
